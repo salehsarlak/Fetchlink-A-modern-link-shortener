78e15235453bf7fe8464f4d44321147be2f60036
Saleh Sarlak <sarlaksaleh7@gmail.com>
2026-09-14 22:40:10 +0330

    Fix port type casting to int

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ $servername = getenv('MYSQLHOST') ?: getenv('DB_HOST') ?: 'localhost';
 $username   = getenv('MYSQLUSER') ?: getenv('DB_USER') ?: 'root';
 $password   = getenv('MYSQLPASSWORD') ?: getenv('DB_PASSWORD') ?: '';
 $dbname     = getenv('MYSQLDATABASE') ?: getenv('DB_NAME') ?: 'link';
-$port       = getenv('MYSQLPORT') ?: 3306;
+$port       = (int) (getenv('MYSQLPORT') ?: getenv('DB_PORT') ?: 3306);
 
 $conn = new mysqli($servername, $username, $password, $dbname, $port);
 
